77e592b37fb2a00c6709771852443622a35611d6
raubenw <raubenw@gmail.com>
2022-03-24 14:55:19 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function create() {
+    
+  let element;
+  let elementList = document.querySelectorAll('.jumbotron input');
+
+  let elementContainer = document.querySelectorAll('.elements');
+
+  if(elementList[0].getAttribute('id') === 'element') {
+	element = document.createElement(elementList[0].value.toLowerCase());
+  }
+
+  if (elementList[1].getAttribute('id') === 'value') {
+    if(elementList[0].value === 'input') {
+        element.value = elementList[1].value;
+    }
+   else {
+       element.innerText = elementList[1].value;
+   }
+  }
+
+  elementContainer[0].appendChild(element);
+
+} 
+
+function createTest() {
+    alert("Hello Werner");
+
+    let jumbotron = document.querySelector(".jumbotron");
+    let h1 = document.createElement("h1");
+    h1.innerText = "Hello Werner";
+
+    jumbotron.appendChild(h1);
+}
